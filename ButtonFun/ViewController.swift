2fff8432c464f19b86a5009b7497364efe841b20
Javier Perez <jperez8089@e104-m-21.mchenry.edu>
2018-02-14 15:08:39 -0600

    initial commit

--- ButtonFun/ViewController.swift
+++ ButtonFun/ViewController.swift
@@ -9,7 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var statusLabel: UILabel!
 
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        
+        
+        let title = sender.title(for: .selected)!
+        let text = "\(title) button pressed"
+        statusLabel.text = text
+        
+        let styledText = NSMutableAttributedString(string: text)
+        let attributes = [
+            NSFontAttributeName: UIFont.boldSystemFont(ofSize: statusLabel.font.pointSize)]
+        
+        let nameRange = (text as NSString).range(of: title)
+        styledText.setAttributes(attributes, range: nameRange)
+        
+        statusLabel.attributedText = styledText
+        
+        
+    }
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
